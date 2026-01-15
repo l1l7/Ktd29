c4094391e2e73a50fe42226c1420e253c467ca2a
l1l7 <rangxr199@gmail.com>
2026-01-15 10:07:41 -0800

    Delete assets/js directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-const accordions = document.querySelectorAll(".accordion");
-
-accordions.forEach(acc => {
-  acc.addEventListener("click", () => {
-    acc.classList.toggle("active");
-    const panel = acc.nextElementSibling;
-    panel.style.display = panel.style.display === "block" ? "none" : "block";
-  });
-});
